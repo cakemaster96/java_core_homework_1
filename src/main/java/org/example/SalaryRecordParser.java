ac755f27f583920874d413a3734b5646cba88afb
icju <isicju@gmail.com>
2026-09-08 20:59:41 +0300

    draft hw1

--- src/main/java/org/example/SalaryRecordParser.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
-
-public interface SalaryRecordParser {
-    List<SalaryRecord> parse(File file) throws IOException;
-}
