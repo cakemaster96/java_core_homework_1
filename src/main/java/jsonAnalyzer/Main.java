4ec027a13e85cfc75069963012b625e485044d86
cakemaster96 <141019594+cakemaster96@users.noreply.github.com>
2026-09-16 09:40:00 +0300

    add blank file validation at analyzer

--- src/main/java/jsonAnalyzer/Main.java
+++ src/main/java/jsonAnalyzer/Main.java
@@ -30,6 +30,12 @@ public class Main {
         for (Path file : files) {
             try {
                 String text = Files.readString(file); //читаем строку из файла
+                if (text.isBlank()) {
+                    System.err.println(
+                            "Пропущен пустой файл: " + file.toAbsolutePath()
+                    );
+                    continue;
+                }
                 List<Employee> employees = mapper.readValue(text, new TypeReference<ArrayList<Employee>>() {});
                 List<Analytic> analytics = analyzer.toAnalytics(employees);
                 String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(analytics);
@@ -40,11 +46,12 @@ public class Main {
                         .replace("_data.txt", "_analytics.txt");
                 Path outputFile = outputDirectory.resolve(outputFileName);
                 Files.write(outputFile, json.getBytes());
-                emailNotificationService.sendEmail("isicjua@gmail.com", messageContent, json.getBytes());
+                emailNotificationService.sendEmail("deviantvector@gmail.com", messageContent, json.getBytes());
                 Path destination = processedDirectory.resolve(file.getFileName());
                 Files.move(file, destination); //Перемещаем прочитанный файл директорию processedDirectory
             }
             catch (IOException e) {
+                System.err.println("Ошибка обработки файла: " + file.toAbsolutePath());
                 e.printStackTrace();
             }
         }
